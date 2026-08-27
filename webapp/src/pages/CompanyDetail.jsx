6f03b0fd21acd44d78ad7fa4daebe55cb3d4f7f4
orfeastops <orfeastops57@gmail.com>
2026-08-27 23:58:35 +0300

    Update CompanyDetail.jsx

--- webapp/src/pages/CompanyDetail.jsx
+++ webapp/src/pages/CompanyDetail.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams, useNavigate, Link } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { api } from '../api';
 import styles from './AdminPanel.module.css';
 
